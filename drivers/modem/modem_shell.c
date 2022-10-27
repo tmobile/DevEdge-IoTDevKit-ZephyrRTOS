f23a2be23f8ed43930160d15a28994311ddf5db3
jtbaumann <103963627+jtbaumann@users.noreply.github.com>
2022-10-27 12:23:39 -0500

    drivers: modem: Added common SMS interface (#23)
    Added common interface for adding SMS support to modem drivers.
    
    Signed-off-by: Jared Baumann <jared.baumann8@t-mobile.com>

--- drivers/modem/modem_shell.c
+++ drivers/modem/modem_shell.c
@@ -6,6 +6,7 @@
 
 /*
  * Copyright (c) 2018 Foundries.io
+ * Copyright (c) 2022 T-Mobile USA, Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -15,11 +16,12 @@
 #include <zephyr/kernel.h>
 #include <stdlib.h>
 #include <string.h>
-#include <zephyr/device.h>
-#include <zephyr/shell/shell.h>
-#include <zephyr/drivers/console/uart_mux.h>
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/console/uart_mux.h>
+#include <zephyr/shell/shell.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/zephyr.h>
 
 struct modem_shell_user_data {
 	const struct shell *shell;
@@ -28,25 +30,24 @@ struct modem_shell_user_data {
 
 #if defined(CONFIG_MODEM_CONTEXT)
 #include "modem_context.h"
-#define ms_context		modem_context
-#define ms_max_context		CONFIG_MODEM_CONTEXT_MAX_NUM
-#define ms_send(ctx_, buf_, size_) \
-			(ctx_->iface.write(&ctx_->iface, buf_, size_))
-#define ms_context_from_id	modem_context_from_id
-#define UART_DEV_NAME(ctx)	(ctx->iface.dev->name)
+#define ms_context     modem_context
+#define ms_max_context CONFIG_MODEM_CONTEXT_MAX_NUM
+#define ms_send(ctx_, buf_, size_)                                             \
+	(ctx_->iface.write(&ctx_->iface, buf_, size_))
+#define ms_context_from_id modem_context_from_id
+#define UART_DEV_NAME(ctx) (ctx->iface.dev->name)
 #elif defined(CONFIG_MODEM_RECEIVER)
 #include "modem_receiver.h"
-#define ms_context		mdm_receiver_context
-#define ms_max_context		CONFIG_MODEM_RECEIVER_MAX_CONTEXTS
-#define ms_send			mdm_receiver_send
-#define ms_context_from_id	mdm_receiver_context_from_id
-#define UART_DEV_NAME(ctx_)	(ctx_->uart_dev->name)
+#define ms_context	    mdm_receiver_context
+#define ms_max_context	    CONFIG_MODEM_RECEIVER_MAX_CONTEXTS
+#define ms_send		    mdm_receiver_send
+#define ms_context_from_id  mdm_receiver_context_from_id
+#define UART_DEV_NAME(ctx_) (ctx_->uart_dev->name)
 #else
 #error "MODEM_CONTEXT or MODEM_RECEIVER need to be enabled"
 #endif
 
-static int cmd_modem_list(const struct shell *shell, size_t argc,
-			  char *argv[])
+static int cmd_modem_list(const struct shell *shell, size_t argc, char *argv[])
 {
 	struct ms_context *mdm_ctx;
 	int i, count = 0;
@@ -57,8 +58,9 @@ static int cmd_modem_list(const struct shell *shell, size_t argc,
 		mdm_ctx = ms_context_from_id(i);
 		if (mdm_ctx) {
 			count++;
-			shell_fprintf(shell, SHELL_NORMAL,
-			     "%d:\tIface Device: %s\n"
+			shell_fprintf(
+				shell, SHELL_NORMAL,
+				"%d:\tIface Device: %s\n"
 				"\tManufacturer: %s\n"
 				"\tModel:        %s\n"
 				"\tRevision:     %s\n"
@@ -74,15 +76,11 @@ static int cmd_modem_list(const struct shell *shell, size_t argc,
 				"\tAcT:          %d\n"
 #endif
 				"\tRSSI:         %d\n",
-			       i,
-			       UART_DEV_NAME(mdm_ctx),
-			       mdm_ctx->data_manufacturer,
-			       mdm_ctx->data_model,
-			       mdm_ctx->data_revision,
-			       mdm_ctx->data_imei,
+				i, UART_DEV_NAME(mdm_ctx),
+				mdm_ctx->data_manufacturer, mdm_ctx->data_model,
+				mdm_ctx->data_revision, mdm_ctx->data_imei,
 #if defined(CONFIG_MODEM_SIM_NUMBERS)
-			       mdm_ctx->data_imsi,
-			       mdm_ctx->data_iccid,
+				mdm_ctx->data_imsi, mdm_ctx->data_iccid,
 #endif
 #if defined(CONFIG_MODEM_CELL_INFO)
 			       mdm_ctx->data_operator,
@@ -90,7 +88,7 @@ static int cmd_modem_list(const struct shell *shell, size_t argc,
 			       mdm_ctx->data_cellid,
 			       mdm_ctx->data_act,
 #endif
-			       mdm_ctx->data_rssi ? *mdm_ctx->data_rssi : 0);
+				mdm_ctx->data_rssi ? *mdm_ctx->data_rssi : 0);
 		}
 	}
 
@@ -101,8 +99,7 @@ static int cmd_modem_list(const struct shell *shell, size_t argc,
 	return 0;
 }
 
-static int cmd_modem_send(const struct shell *shell, size_t argc,
-			  char *argv[])
+static int cmd_modem_send(const struct shell *shell, size_t argc, char *argv[])
 {
 	struct ms_context *mdm_ctx;
 	char *endptr;
@@ -178,9 +175,8 @@ static void uart_mux_cb(const struct device *uart, const struct device *dev,
 		ch = "control";
 	}
 
-	shell_fprintf(shell, SHELL_NORMAL,
-		      "%s\t\t%s\t\t%d (%s)\n",
-		      uart->name, dev->name, dlci_address, ch);
+	shell_fprintf(shell, SHELL_NORMAL, "%s\t\t%s\t\t%d (%s)\n", uart->name,
+		      dev->name, dlci_address, ch);
 }
 #endif
 
@@ -219,16 +215,12 @@ static int cmd_modem_info(const struct shell *shell, size_t argc, char *argv[])
 		      "Revision         : %s\n"
 		      "IMEI             : %s\n"
 		      "RSSI             : %d\n",
-		      i,
-		      UART_DEV_NAME(mdm_ctx),
-		      mdm_ctx->data_manufacturer,
-		      mdm_ctx->data_model,
-		      mdm_ctx->data_revision,
+		      i, UART_DEV_NAME(mdm_ctx), mdm_ctx->data_manufacturer,
+		      mdm_ctx->data_model, mdm_ctx->data_revision,
 		      mdm_ctx->data_imei,
 		      mdm_ctx->data_rssi ? *mdm_ctx->data_rssi : 0);
 
-	shell_fprintf(shell, SHELL_NORMAL,
-		      "GSM 07.10 muxing : %s\n",
+	shell_fprintf(shell, SHELL_NORMAL, "GSM 07.10 muxing : %s\n",
 		      IS_ENABLED(CONFIG_GSM_MUX) ? "enabled" : "disabled");
 
 #if defined(CONFIG_GSM_MUX)
@@ -244,11 +236,120 @@ static int cmd_modem_info(const struct shell *shell, size_t argc, char *argv[])
 	return 0;
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_modem,
+static int cmd_modem_sms_send(const struct shell *shell, size_t argc,
+			      char *argv[])
+{
+	struct ms_context *ms_ctx;
+	struct sms_out sms;
+	int ret = -1;
+
+	if (argc != 4) {
+		shell_fprintf(shell, SHELL_ERROR,
+			      "usage: modem sms send <modem index> <phone "
+			      "number> <message>\n");
+	} else {
+		int modem_index = atoi(argv[1]);
+
+		ms_ctx = ms_context_from_id(modem_index);
+		if (ms_ctx == NULL) {
+			shell_fprintf(
+				shell, SHELL_ERROR,
+				"Couldn't open context for modem index %d\n",
+				modem_index);
+			return -ENOEXEC;
+		}
+
+		if (ms_ctx->send_sms == NULL) {
+			shell_fprintf(shell, SHELL_ERROR,
+				      "No function defined to send SMS\n");
+			return -ENOEXEC;
+		}
+
+		snprintk(sms.phone, sizeof(sms.phone), "%s", argv[2]);
+		snprintk(sms.msg, sizeof(sms.msg), "%s", argv[3]);
+
+		ret = ms_ctx->send_sms(ms_ctx, &sms);
+		if (ret == 0) {
+			shell_fprintf(shell, SHELL_NORMAL,
+				      "SMS msg was sent\n");
+		} else {
+			shell_fprintf(shell, SHELL_ERROR,
+				      "error: send_sms returned %d\n", ret);
+		}
+	}
+	return ret;
+}
+
+static int cmd_modem_sms_recv(const struct shell *shell, size_t argc,
+			      char *argv[])
+{
+	struct ms_context *ms_ctx;
+	struct sms_in sms;
+	int ret = -1;
+
+	if (argc != 3) {
+		shell_fprintf(shell, SHELL_ERROR,
+			      "usage: modem sms recv <modem index> <wait time "
+			      "(seconds)>\n");
+	} else {
+		int modem_index = atoi(argv[1]);
+		int wait = atoi(argv[2]);
+
+		ms_ctx = ms_context_from_id(modem_index);
+		if (ms_ctx == NULL) {
+			shell_fprintf(
+				shell, SHELL_ERROR,
+				"Couldn't open context for modem index %d\n",
+				modem_index);
+			return -ENOEXEC;
+		}
+
+		if (ms_ctx->recv_sms == NULL) {
+			shell_fprintf(shell, SHELL_ERROR,
+				      "No function defined to receive SMS\n");
+			return -ENOEXEC;
+		}
+
+		sms.timeout = K_SECONDS(wait);
+		ret = ms_ctx->recv_sms(ms_ctx, &sms);
+		if (ret < 0) {
+			shell_fprintf(shell, SHELL_ERROR,
+				      "recv_sms returned error %d, errno:%d\n",
+				      ret, errno);
+		} else if (ret == 0) {
+			shell_fprintf(shell, SHELL_NORMAL,
+				      "No SMS msgs available\n");
+		} else {
+			shell_fprintf(
+				shell, SHELL_NORMAL,
+				"Received SMS msg from %s dated %s: '%s'\n",
+				sms.phone, sms.time, sms.msg);
+		}
+	}
+	return ret;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(modem_cmd_sms,
+			       SHELL_CMD(send, NULL,
+					 "'modem sms send <modem index> <phone "
+					 "number> <message>' send sms message",
+					 cmd_modem_sms_send),
+			       SHELL_CMD(recv, NULL,
+					 "'modem sms recv <modem index> <wait "
+					 "time (seconds)>' receive sms message",
+					 cmd_modem_sms_recv),
+			       SHELL_SUBCMD_SET_END);
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sub_modem,
 	SHELL_CMD(info, NULL, "Show information for a modem", cmd_modem_info),
 	SHELL_CMD(list, NULL, "List registered modems", cmd_modem_list),
-	SHELL_CMD(send, NULL, "Send an AT <command> to a registered modem "
-			      "receiver", cmd_modem_send),
+	SHELL_CMD(send, NULL,
+		  "Send an AT <command> to a registered modem "
+		  "receiver",
+		  cmd_modem_send),
+	SHELL_CMD(sms, &modem_cmd_sms, "Send or receive SMS message via modem",
+		  NULL),
 	SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 
---
+++ drivers/modem/modem_sms.h
@@ -0,0 +1,45 @@
+/** @file
+ * @brief Modem SMS for SMS common structure.
+ *
+ * Modem SMS handling for modem driver.
+ */
+
+/*
+ * Copyright (c) 2022 T-Mobile USA, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_MODEM_MODEM_SMS_H_
+#define ZEPHYR_INCLUDE_DRIVERS_MODEM_MODEM_SMS_H_
+
+#define SMS_PHONE_MAX_LEN 16
+#define SMS_TIME_MAX_LEN  26
+
+/*
+ * All fields in sms_out and sms_in are NULL terminated
+ */
+
+struct sms_out {
+	char phone[SMS_PHONE_MAX_LEN];
+	char msg[CONFIG_MODEM_SMS_OUT_MSG_MAX_LEN + 1];
+};
+
+struct sms_in {
+	char phone[SMS_PHONE_MAX_LEN];
+	char time[SMS_TIME_MAX_LEN];
+	char msg[CONFIG_MODEM_SMS_IN_MSG_MAX_LEN + 1];
+	uint8_t csms_ref;
+	uint8_t csms_idx;
+	k_timeout_t timeout;
+};
+
+typedef int (*send_sms_func_t)(void *obj, const struct sms_out *sms);
+typedef int (*recv_sms_func_t)(void *obj, struct sms_in *sms);
+
+enum io_ctl {
+	SMS_SEND,
+	SMS_RECV,
+};
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_MODEM_MODEM_SMS_H_ */
