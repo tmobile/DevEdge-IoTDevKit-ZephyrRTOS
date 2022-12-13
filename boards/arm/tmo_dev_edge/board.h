cc8606977203bc8ad400559adadcab53575ca698
Jared Baumann <jared.baumann8@t-mobile.com>
2022-12-13 12:21:02 -0700

    boards: arm: Fix tmo_dev_edge build
    Fixed issue preventing dev_edge board from building and samples from
    running.
    
    Signed-off-by: Jared Baumann <jared.baumann8@t-mobile.com>

--- boards/arm/tmo_dev_edge/board.h
+++ boards/arm/tmo_dev_edge/board.h
@@ -8,13 +8,15 @@
 #ifndef __INC_BOARD_H
 #define __INC_BOARD_H
 
+#include <zephyr/devicetree.h>
+
 /* These pins is used as part of the 9116 initialization routine */
-#define RS9116_GPIO_NAME  "GPIO_A"
+#define RS9116_GPIO_NAME  "gpio@4000a000" /* GPIO_A */
 #define RS9116_POC_GPIO_PIN   8
 #define RS9116_RST_GPIO_PIN   9
 
 /* These pins are used as part of the PAM8904E initialization routine */
-#define BUZZER_ENx_GPIO_NAME  "GPIO_C"
+#define BUZZER_ENx_GPIO_NAME  "gpio@4000a060" /* GPIO_C */
 #define BUZZER_EN1_GPIO_PIN   4
 #define BUZZER_EN2_GPIO_PIN   5
 
@@ -24,14 +26,19 @@
 #define GREEN_LED_GPIO_PIN    9
 #define BLUE_LED_GPIO_PIN     10
 
-#define GNSS_GPIO_NAME "GPIO_F"
+#define GNSS_GPIO_NAME "gpio@4000a0f0" /* GPIO_F */
 #define GNSS_BOOT_REQ 6
 #define GNSS_RESET 8
 #define GNSS_PWR_ON 10
 
 #define BQ_CHIP_ENABLE 13
 
-#define LED_PWM_NAME "PWM_4"
+#if DT_NODE_EXISTS(DT_NODELABEL(pwm1))
+#define LED_PWM_DEV DEVICE_DT_GET(DT_NODELABEL(pwm1))
+#else
+#define LED_PWM_DEV NULL
+#endif
+
 #define LED_PWM_WHITE 0
 #define LED_PWM_RED   1
 #define LED_PWM_GREEN 2
