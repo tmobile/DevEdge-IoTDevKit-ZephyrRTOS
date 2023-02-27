8b1e2b8158d1e99452ea2d4e635c2ecab979b9fe
Dennis Ruffer <Dennis.Ruffer1@T-Mobile.com>
2023-02-27 14:53:17 -0800

    drivers: counter: Remove TIME_GECKO_RTCC and doc/finish persistence
    Remove samples/drivers/counter/gecko_rtcc folder
    Fix compliance issues in UART Bootloader devices
    
    Signed-off-by: Dennis Ruffer <Dennis.Ruffer1@T-Mobile.com>

--- drivers/counter/counter_gecko_rtcc.c
+++ drivers/counter/counter_gecko_rtcc.c
@@ -247,8 +247,8 @@ static int update_posix(void)
  *
  * Here is a list of the devices supported by the UART Bootloader:
  * See: https://www.silabs.com/documents/public/application-notes/an0003-efm32-uart-bootloader.pdf
-*/
-#if (	/* EFM32 Series 0: */ \
+ */
+#if    (/* EFM32 Series 0: */ \
 	defined(CONFIG_SOC_SERIES_EFM32G)	/*!< EFM32 Gecko */ || \
 	defined(CONFIG_SOC_SERIES_EFM32GG)	/*!< EFM32 Giant Gecko */ || \
 	defined(CONFIG_SOC_SERIES_EFM32WG)	/*!< EFM32 Wonder Gecko */ || \
@@ -267,7 +267,7 @@ static int update_posix(void)
 	defined(CONFIG_SOC_SERIES_EFM32JG12)	/*!< EFM32 Jade Gecko (Rev C onwards) */ || \
 	defined(CONFIG_SOC_SERIES_EFM32GG11)	/*!< EFM32 Giant Gecko GG11 */ || \
 	defined(CONFIG_SOC_SERIES_EFM32GG12)	/*!< EFM32 Giant Gecko GG12 */ || \
-	defined(CONFIG_SOC_SERIES_EFM32TG11)	/*!< EFM32 Tiny Gecko 11 */ )
+	defined(CONFIG_SOC_SERIES_EFM32TG11)	/*!< EFM32 Tiny Gecko 11 */)
 #define LB_CLW0           (*((volatile uint32_t *)(LOCKBITS_BASE)+122))
 #define LB_CLW0_PINBOOTLOADER    (1 << 1)
 #endif /* EFM32, EZR32 SOC Series */
