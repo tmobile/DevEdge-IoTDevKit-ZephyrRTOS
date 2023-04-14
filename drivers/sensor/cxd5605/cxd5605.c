0102e49c36da14e5a4eb7e2a008348e458c09c68
jtbaumann <103963627+jtbaumann@users.noreply.github.com>
2023-04-14 17:34:45 -0500

    drivers: Added initial support for power domains (#169)
    Added needed functionality for power domains.
    
    Signed-off-by: Jared Baumann <jared.baumann8@t-mobile.com>

--- drivers/sensor/cxd5605/cxd5605.c
+++ drivers/sensor/cxd5605/cxd5605.c
@@ -692,7 +692,6 @@ static int cxd5605_driver_pm_action(const struct device *dev,
 		 * powered on the device, used when the power
 		 * domain this device belongs is resumed.
 		 */
-		result = gpio_pin_configure_dt(pwr_gpio, GPIO_OUTPUT_HIGH);
 		result = gpio_pin_configure_dt(rst_gpio, GPIO_OUTPUT_HIGH);
 		k_msleep(3000);	/* wait for GNSS chip to boot */
 		result = init(dev);
@@ -713,7 +712,6 @@ static int cxd5605_driver_pm_action(const struct device *dev,
 		 */
 		cxd5605_sleep(dev, 2);
 		gpio_pin_interrupt_configure_dt(&config->int_gpio, GPIO_INT_DISABLE);
-		result = gpio_pin_configure_dt(pwr_gpio, GPIO_OUTPUT_LOW);
 		result = gpio_pin_configure_dt(rst_gpio, GPIO_OUTPUT_LOW);
 		break;
 	default:
