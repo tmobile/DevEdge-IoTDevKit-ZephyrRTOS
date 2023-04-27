zephyrproject-rtos/zephyr

drivers: timer: get mtime cmp reg by reading mhartid
It is not guaranteed that a multi-core RISC-V hart numbering scheme
will match Zephyr's sequential cpu numbering scheme. Read the hartid and
use that value in calculation to get mtime_cmp reg, instead of the
current_cpu id.

Signed-off-by: Conor Paxton <conor.paxton@microchip.com>

drivers/timer/riscv_machine_timer.c
@@ -67,7 +67,7 @@ const int32_t z_sys_timer_irq_for_test = TIMER_IRQN;
 
 static uint64_t get_hart_mtimecmp(void)
 {
-	return MTIMECMP_REG + (_current_cpu->id * 8);
+	return MTIMECMP_REG + (arch_proc_id() * 8);
 }
 
 static void set_mtimecmp(uint64_t time)

tmobile/DevEdge-IoTDevKit-ZephyrRTOS

(cherry picked from commit 6b641c3483b3be3351c39c50d5c8aef685407ed9)

drivers/timer/riscv_machine_timer.c
@@ -21,7 +21,7 @@ static uint64_t last_count;
 
 static uint64_t get_hart_mtimecmp(void)
 {
-	return RISCV_MTIMECMP_BASE + (_current_cpu->id * 8);
+	return RISCV_MTIMECMP_BASE + (arch_proc_id() * 8);
 }
 
 static void set_mtimecmp(uint64_t time)