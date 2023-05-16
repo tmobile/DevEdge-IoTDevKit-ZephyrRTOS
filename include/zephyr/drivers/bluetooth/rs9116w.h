29c3274a10aec5b37f18d6025272b09083271505
Jared Baumann <jared.baumann8@t-mobile.com>
2023-05-16 13:47:46 +0000

    drivers: bluetooth: Add MAC read to 9116 driver
    Added ability to read MAC address to RS9116 BLE driver.
    
    Signed-off-by: Jared Baumann <jared.baumann8@t-mobile.com>

--- include/zephyr/drivers/bluetooth/rs9116w.h
+++ include/zephyr/drivers/bluetooth/rs9116w.h
@@ -8,6 +8,14 @@
  * @brief Get the RSSI of a connection
  *
  * @param conn Connection to read RSSI for
- * @return int Absolute value of RSSI on sucxess, negative errno on fail.
+ * @return int Absolute value of RSSI on success, negative errno on fail.
  */
 int bt_conn_le_get_rssi(const struct bt_conn *conn);
+
+/**
+ * @brief Get the hadware MAC address from the bluetooth hardware
+ *
+ * @param mac Output buffer for the MAC address
+ * @return int 0 on success, negative errno on failure.
+ */
+int bt_get_mac(char *mac);
