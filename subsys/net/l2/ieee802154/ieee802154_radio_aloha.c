66be010c3c90d9b4dbcc4c442753045da32dfda2
Florian Grandel <fgrandel@code-for-humans.de>
2023-05-31 13:42:44 +0200

    net: l2: ieee802154: radio: fix aloha algorithm
    The ALOHA algorithm had two minor implementation errors:
    
    - The number of retransmissions was off by one.
    
    - Retransmissions are only allowed when acknowledgement is requested
      otherwise it is to be assumed that the transmission was successful.
    
    - prepare_for_ack() has side effects and must be called before each
      retransmission.
    
    Signed-off-by: Florian Grandel <fgrandel@code-for-humans.de>

--- subsys/net/l2/ieee802154/ieee802154_radio_aloha.c
+++ subsys/net/l2/ieee802154/ieee802154_radio_aloha.c
@@ -11,7 +11,37 @@ LOG_MODULE_REGISTER(net_ieee802154_aloha, CONFIG_NET_L2_IEEE802154_LOG_LEVEL);
 
 #include <zephyr/net/net_if.h>
 
-#include "ieee802154_priv.h"
+#include <errno.h>
+
+#include "ieee802154_frame.h"
+#include "ieee802154_utils.h"
+#include "ieee802154_radio_utils.h"
+
+static inline int aloha_radio_send(struct net_if *iface,
+				   struct net_pkt *pkt,
+				   struct net_buf *frag)
+{
+	/* See section 6.7.4.4 - Retransmissions. */
+	uint8_t remaining_attempts = CONFIG_NET_L2_IEEE802154_RADIO_TX_RETRIES + 1;
+	struct ieee802154_context *ctx = net_if_l2_data(iface);
+	bool ack_required;
+	int ret = -EIO;
+
+	NET_DBG("frag %p", frag);
+
+	while (remaining_attempts) {
+		remaining_attempts--;
+
+		ack_required = prepare_for_ack(ctx, pkt, frag);
+
+		if (!ack_required) {
+			/* See section 6.7.4.4: "A device that sends a frame with the AR field set
+			 * to indicate no acknowledgment requested may assume that the transmission
+			 * was successfully received and shall not perform the retransmission
+			 * procedure."
+			 */
+			remaining_attempts = 0;
+		}
 
 static inline int aloha_channel_access(struct net_if *iface)
 {
