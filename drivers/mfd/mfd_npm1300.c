035d4a00d50bf76f34e13a2ca9f0e18c946d6bac
Andy Sinclair <andy.sinclair@nordicsemi.no>
2023-05-31 12:52:54 +0100

    drivers: mfd: npm1300: Initial version
    Added an MFD driver for the nPM1300.
    This driver has register access helper functions that can be used
    by subsystems.  This will avoid each subsystem having to duplicate
    the register access code.
    
    Signed-off-by: Andy Sinclair <andy.sinclair@nordicsemi.no>

--- drivers/mfd/mfd_npm1300.c
+++ drivers/mfd/mfd_npm1300.c
@@ -9,22 +9,7 @@
 
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/sys/util.h>
-#include <zephyr/sys/byteorder.h>
 #include <zephyr/drivers/mfd/npm1300.h>
-
-#define TIME_BASE 0x07U
-#define MAIN_BASE 0x00U
-#define SHIP_BASE 0x0BU
-
-#define TIME_OFFSET_LOAD  0x03U
-#define TIME_OFFSET_TIMER 0x08U
-
-#define MAIN_OFFSET_RESET 0x01U
-
-#define SHIP_OFFSET_HIBERNATE 0x00U
-
-#define TIMER_PRESCALER_MS 16U
-#define TIMER_MAX          0xFFFFFFU
 
 struct mfd_npm1300_config {
 	struct i2c_dt_spec i2c;
@@ -52,9 +37,17 @@ int mfd_npm1300_reg_read_burst(const struct device *dev, uint8_t base, uint8_t o
 			       size_t len)
 {
 	const struct mfd_npm1300_config *config = dev->config;
+	struct mfd_npm1300_data *mfd_data = dev->data;
 	uint8_t buff[] = {base, offset};
+	int ret;
 
-	return i2c_write_read_dt(&config->i2c, buff, sizeof(buff), data, len);
+	k_mutex_lock(&mfd_data->mutex, K_FOREVER);
+
+	ret = i2c_write_read_dt(&config->i2c, buff, sizeof(buff), data, len);
+
+	k_mutex_unlock(&mfd_data->mutex);
+
+	return ret;
 }
 
 int mfd_npm1300_reg_read(const struct device *dev, uint8_t base, uint8_t offset, uint8_t *data)
@@ -65,18 +58,34 @@ int mfd_npm1300_reg_read(const struct device *dev, uint8_t base, uint8_t offset,
 int mfd_npm1300_reg_write(const struct device *dev, uint8_t base, uint8_t offset, uint8_t data)
 {
 	const struct mfd_npm1300_config *config = dev->config;
+	struct mfd_npm1300_data *mfd_data = dev->data;
 	uint8_t buff[] = {base, offset, data};
+	int ret;
 
-	return i2c_write_dt(&config->i2c, buff, sizeof(buff));
+	k_mutex_lock(&mfd_data->mutex, K_FOREVER);
+
+	ret = i2c_write_dt(&config->i2c, buff, sizeof(buff));
+
+	k_mutex_unlock(&mfd_data->mutex);
+
+	return ret;
 }
 
 int mfd_npm1300_reg_write2(const struct device *dev, uint8_t base, uint8_t offset, uint8_t data1,
 			   uint8_t data2)
 {
 	const struct mfd_npm1300_config *config = dev->config;
+	struct mfd_npm1300_data *mfd_data = dev->data;
 	uint8_t buff[] = {base, offset, data1, data2};
+	int ret;
 
-	return i2c_write_dt(&config->i2c, buff, sizeof(buff));
+	k_mutex_lock(&mfd_data->mutex, K_FOREVER);
+
+	ret = i2c_write_dt(&config->i2c, buff, sizeof(buff));
+
+	k_mutex_unlock(&mfd_data->mutex);
+
+	return ret;
 }
 
 int mfd_npm1300_reg_update(const struct device *dev, uint8_t base, uint8_t offset, uint8_t data,
@@ -98,43 +107,6 @@ int mfd_npm1300_reg_update(const struct device *dev, uint8_t base, uint8_t offse
 	k_mutex_unlock(&mfd_data->mutex);
 
 	return ret;
-}
-
-int mfd_npm1300_set_timer(const struct device *dev, uint32_t time_ms)
-{
-	const struct mfd_npm1300_config *config = dev->config;
-	uint8_t buff[5] = {TIME_BASE, TIME_OFFSET_TIMER};
-	uint32_t ticks = time_ms / TIMER_PRESCALER_MS;
-
-	if (ticks > TIMER_MAX) {
-		return -EINVAL;
-	}
-
-	sys_put_be24(ticks, &buff[2]);
-
-	int ret = i2c_write_dt(&config->i2c, buff, sizeof(buff));
-
-	if (ret != 0) {
-		return ret;
-	}
-
-	return mfd_npm1300_reg_write(dev, TIME_BASE, TIME_OFFSET_LOAD, 1U);
-}
-
-int mfd_npm1300_reset(const struct device *dev)
-{
-	return mfd_npm1300_reg_write(dev, MAIN_BASE, MAIN_OFFSET_RESET, 1U);
-}
-
-int mfd_npm1300_hibernate(const struct device *dev, uint32_t time_ms)
-{
-	int ret = mfd_npm1300_set_timer(dev, time_ms);
-
-	if (ret != 0) {
-		return ret;
-	}
-
-	return mfd_npm1300_reg_write(dev, SHIP_BASE, SHIP_OFFSET_HIBERNATE, 1U);
 }
 
 #define MFD_NPM1300_DEFINE(inst)                                                                   \
