573287f74c1e23f927e3c1084b394409c5abe8fb
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2023-06-14 12:35:14 +0200

    device: s/DEVICE_HANDLE/DEVICE_DEPS/
    Rename multiple internal device macros to use the DEVICE_DEPS naming, so
    that it is clear they belong to the device dependencies APIs.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- include/zephyr/device.h
+++ include/zephyr/device.h
@@ -30,20 +30,16 @@ extern "C" {
 /** @cond INTERNAL_HIDDEN */
 
 /**
- * @brief Flag value used in lists of device handles to separate distinct
+ * @brief Flag value used in lists of device dependencies to separate distinct
  * groups.
- *
- * This is the minimum value for the device_handle_t type.
  */
-#define Z_DEVICE_HANDLE_SEP INT16_MIN
+#define Z_DEVICE_DEPS_SEP INT16_MIN
 
 /**
- * @brief Flag value used in lists of device handles to indicate the end of the
- * list.
- *
- * This is the maximum value for the device_handle_t type.
+ * @brief Flag value used in lists of device dependencies to indicate the end of
+ * the list.
  */
-#define Z_DEVICE_HANDLE_ENDS INT16_MAX
+#define Z_DEVICE_DEPS_ENDS INT16_MAX
 
 /** @endcond */
 
@@ -372,7 +368,7 @@ struct device_state {
 
 struct pm_device;
 
-#ifdef CONFIG_DEVICE_DEPS_DYNAMIC
+#ifdef CONFIG_HAS_DYNAMIC_DEVICE_HANDLES
 #define Z_DEVICE_DEPS_CONST
 #else
 #define Z_DEVICE_DEPS_CONST const
@@ -401,8 +397,8 @@ struct device {
 	 * dedicated API, such as device_required_handles_get(). Only available
 	 * if @kconfig{CONFIG_DEVICE_DEPS} is enabled.
 	 */
-	Z_DEVICE_DEPS_CONST device_handle_t *deps;
-#endif /* CONFIG_DEVICE_DEPS */
+	Z_DEVICE_DEPS_CONST device_handle_t *handles;
+
 #if defined(CONFIG_PM_DEVICE) || defined(__DOXYGEN__)
 	/**
 	 * Reference to the device PM resources (only available if
@@ -508,8 +504,8 @@ device_required_handles_get(const struct device *dev, size_t *count)
 	if (rv != NULL) {
 		size_t i = 0;
 
-		while ((rv[i] != Z_DEVICE_HANDLE_ENDS) &&
-		       (rv[i] != Z_DEVICE_HANDLE_SEP)) {
+		while ((rv[i] != Z_DEVICE_DEPS_ENDS) &&
+		       (rv[i] != Z_DEVICE_DEPS_SEP)) {
 			++i;
 		}
 		*count = i;
@@ -546,13 +542,13 @@ device_injected_handles_get(const struct device *dev, size_t *count)
 	if (rv != NULL) {
 		/* Fast forward to injected devices */
 		while (region != 1) {
-			if (*rv == Z_DEVICE_HANDLE_SEP) {
+			if (*rv == Z_DEVICE_DEPS_SEP) {
 				region++;
 			}
 			rv++;
 		}
-		while ((rv[i] != Z_DEVICE_HANDLE_ENDS) &&
-		       (rv[i] != Z_DEVICE_HANDLE_SEP)) {
+		while ((rv[i] != Z_DEVICE_DEPS_ENDS) &&
+		       (rv[i] != Z_DEVICE_DEPS_SEP)) {
 			++i;
 		}
 		*count = i;
@@ -590,7 +586,7 @@ device_supported_handles_get(const struct device *dev, size_t *count)
 	if (rv != NULL) {
 		/* Fast forward to supporting devices */
 		while (region != 2) {
-			if (*rv == Z_DEVICE_HANDLE_SEP) {
+			if (*rv == Z_DEVICE_DEPS_SEP) {
 				region++;
 			}
 			rv++;
@@ -599,7 +595,7 @@ device_supported_handles_get(const struct device *dev, size_t *count)
 		 * Trailing NULL's can be injected by gen_device_deps.py due to
 		 * CONFIG_PM_DEVICE_POWER_DOMAIN_DYNAMIC_NUM
 		 */
-		while ((rv[i] != Z_DEVICE_HANDLE_ENDS) &&
+		while ((rv[i] != Z_DEVICE_DEPS_ENDS) &&
 		       (rv[i] != DEVICE_HANDLE_NULL)) {
 			++i;
 		}
@@ -778,7 +774,7 @@ static inline bool z_impl_device_is_ready(const struct device *dev)
  *
  * @param dev_id Device identifier.
  */
-#define Z_DEVICE_DEPS_NAME(dev_id) _CONCAT(__devicedeps_, dev_id)
+#define Z_DEVICE_DEPS_NAME(dev_id) _CONCAT(__devicehdl_, dev_id)
 
 /**
  * @brief Expand extra dependencies with a comma in between.
@@ -788,8 +784,8 @@ static inline bool z_impl_device_is_ready(const struct device *dev)
 #define Z_DEVICE_EXTRA_DEPS(...)                                            \
 	FOR_EACH_NONEMPTY_TERM(IDENTITY, (,), __VA_ARGS__)
 
-/** @brief Linker section were device handles are placed. */
-#define Z_DEVICE_HANDLES_SECTION                                               \
+/** @brief Linker section were device dependencies are placed. */
+#define Z_DEVICE_DEPS_SECTION                                               \
 	__attribute__((__section__(".__device_deps_pass1")))
 
 #ifdef __cplusplus
@@ -811,18 +807,18 @@ static inline bool z_impl_device_is_ready(const struct device *dev)
  * {
  *     DEVICE_ORDINAL (or DEVICE_HANDLE_NULL if not a devicetree node),
  *     List of devicetree dependency ordinals (if any),
- *     Z_DEVICE_HANDLE_SEP,
+ *     Z_DEVICE_DEPS_SEP,
  *     List of injected dependency ordinals (if any),
- *     Z_DEVICE_HANDLE_SEP,
+ *     Z_DEVICE_DEPS_SEP,
  *     List of devicetree supporting ordinals (if any),
  * }
  *
  * After processing in gen_device_deps.py, the format is updated to:
  * {
  *     List of existing devicetree dependency handles (if any),
- *     Z_DEVICE_HANDLE_SEP,
+ *     Z_DEVICE_DEPS_SEP,
  *     List of injected devicetree dependency handles (if any),
- *     Z_DEVICE_HANDLE_SEP,
+ *     Z_DEVICE_DEPS_SEP,
  *     List of existing devicetree support handles (if any),
  *     DEVICE_HANDLE_NULL
  * }
@@ -843,9 +839,9 @@ static inline bool z_impl_device_is_ready(const struct device *dev)
 			DT_NODE_EXISTS(node_id),                               \
 			(DT_DEP_ORD(node_id), DT_REQUIRES_DEP_ORDS(node_id)),  \
 			(DEVICE_HANDLE_NULL,)) /**/                            \
-		Z_DEVICE_HANDLE_SEP,                                           \
-		Z_DEVICE_EXTRA_HANDLES(__VA_ARGS__) /**/                       \
-		Z_DEVICE_HANDLE_SEP,                                           \
+		Z_DEVICE_DEPS_SEP,                                             \
+		Z_DEVICE_EXTRA_DEPS(__VA_ARGS__) /**/                          \
+		Z_DEVICE_DEPS_SEP,                                             \
 		COND_CODE_1(DT_NODE_EXISTS(node_id),                           \
 			    (DT_SUPPORTS_DEP_ORDS(node_id)), ()) /**/          \
 	}
@@ -965,8 +961,7 @@ static inline bool z_impl_device_is_ready(const struct device *dev)
 			level, prio, api, state, ...)                          \
 	Z_DEVICE_NAME_CHECK(name);                                             \
                                                                                \
-	IF_ENABLED(CONFIG_DEVICE_DEPS,                                         \
-		   (Z_DEVICE_DEPS_DEFINE(node_id, dev_id, __VA_ARGS__);))      \
+	Z_DEVICE_DEPS_DEFINE(node_id, dev_id, __VA_ARGS__);                    \
                                                                                \
 	Z_DEVICE_BASE_DEFINE(node_id, dev_id, name, pm, data, config, level,   \
 			     prio, api, state, Z_DEVICE_DEPS_NAME(dev_id));    \
