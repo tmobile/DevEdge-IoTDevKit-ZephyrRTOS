1e94d61675b88380c45124891e07c4c0e9ed20b6
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-06-25 21:18:19 +0530

    net: wifi: Add an error code for TWT duplicate flow request
    This is to handle duplicate TWT setup request.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- include/zephyr/net/wifi.h
+++ include/zephyr/net/wifi.h
@@ -432,6 +432,7 @@ enum wifi_twt_fail_reason {
 	/** Invalid negotiated flow id */
 	WIFI_TWT_FAIL_INVALID_FLOW_ID,
 	WIFI_TWT_FAIL_IP_NOT_ASSIGNED,
+	WIFI_TWT_FAIL_FLOW_ALREADY_EXISTS,
 };
 
 static const char * const twt_err_code_tbl[] = {
@@ -451,6 +452,8 @@ static const char * const twt_err_code_tbl[] = {
 		"Invalid negotiated flow id",
 	[WIFI_TWT_FAIL_IP_NOT_ASSIGNED] =
 		"IP address not assigned",
+	[WIFI_TWT_FAIL_FLOW_ALREADY_EXISTS] =
+		"Flow already exists",
 };
 
 /** Helper function to get user-friendly TWT error code name. */
