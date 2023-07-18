b0068ed15effbdd39e19f0c5d70bd592542c71ce
Lukasz Mrugala <lukaszx.mrugala@intel.com>
2023-07-18 14:56:22 +0200

    scripts: pylib: twister: twisterlib: environment: TwisterEnv fixes
    Fixes Issue #60522 by guarding member access against None parameters.
    Shortens the init by using the if...else assignment.
    Fixes two typos.
    
    Signed-off-by: Lukasz Mrugala <lukaszx.mrugala@intel.com>

--- scripts/pylib/twister/twisterlib/environment.py
+++ scripts/pylib/twister/twisterlib/environment.py
@@ -791,6 +791,7 @@ class TwisterEnv:
         self.commit_date = None
         self.run_date = None
         self.options = options
+
         if options and options.ninja:
             self.generator_cmd = "ninja"
             self.generator = "Ninja"
@@ -799,10 +800,8 @@ class TwisterEnv:
             self.generator = "Unix Makefiles"
         logger.info(f"Using {self.generator}..")
 
-        if options:
-            self.test_roots = options.testsuite_root
-        else:
-            self.test_roots = None
+        self.test_roots = options.testsuite_root if options else None
+
         if options:
             if not isinstance(options.board_root, list):
                 self.board_roots = [self.options.board_root]
@@ -815,9 +814,9 @@ class TwisterEnv:
 
         self.hwm = None
 
-        self.test_config = options.test_config
+        self.test_config = options.test_config if options else None
 
-        self.alt_config_root = options.alt_config_root
+        self.alt_config_root = options.alt_config_root if options else None
 
     def discover(self):
         self.check_zephyr_version()
@@ -837,7 +836,7 @@ class TwisterEnv:
                     logger.info(f"Zephyr version: {self.version}")
                 else:
                     self.version = "Unknown"
-                    logger.error("Coult not determine version")
+                    logger.error("Could not determine version")
         except OSError:
             logger.info("Cannot read zephyr version.")
 
@@ -883,7 +882,7 @@ class TwisterEnv:
         out = ansi_escape.sub('', out.decode())
 
         if p.returncode == 0:
-            msg = "Finished running  %s" % (args[0])
+            msg = "Finished running %s" % (args[0])
             logger.debug(msg)
             results = {"returncode": p.returncode, "msg": msg, "stdout": out}
 
